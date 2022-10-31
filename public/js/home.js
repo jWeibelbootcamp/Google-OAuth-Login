389b8d9ea76f14a574247f1521f18e5f4b6f757d
Noelle Darcy <noellerdarcy@gmail.com>
2022-10-31 13:46:16 -0400

    working with Sally

--- public/js/home.js
+++ public/js/home.js
@@ -1,11 +1,28 @@
+// const loginBtn = document.getElementById("login-btn");
+// loginBtn.addEventListener('submit', loginFunction);
+// const loginFunction = function(event) {
+//     // event.preventDefault();
+//     // const response = await fetch("/login", {method:"GET"});
+//     // if(response.ok) {
+//     //     // document.location.replace();
+//     //     console.log("okay");
+//     // }
+//     // document.location.replace('/login');
+//     window.open("/login");
+// }
+// // function loginFunction (event) {
+// //     // fetch.request("login")
+// //     const response = 
+// // };
+
+// const registerBtn = document.getElementById("register-btn");
+// registerBtn.addEventListener('click', registerFunction);
+// function registerFunction (event) {
+//     fetch.request("register")
+// };
+
 const loginBtn = document.getElementById("login-btn");
 loginBtn.addEventListener('click', loginFunction);
 function loginFunction (event) {
-    res.render("login")
-};
-
-const registerBtn = document.getElementById("register-btn");
-registerBtn.addEventListener('click', registerFunction);
-function registerFunction (event) {
-    res.render("register")
+    window.open("/login");
 };
