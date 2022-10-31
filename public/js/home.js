60fb354b3544eebc6d4b3919d746c2cf9351c333
devWeibel <weibel.jason@gmail.com>
2022-10-31 11:18:00 -0400

    still workin

---
+++ public/js/home.js
@@ -0,0 +1,11 @@
+const loginBtn = document.getElementById("grooming");
+loginBtn.addEventListener('click', grooming);
+function grooming(event) {
+    window.open("layouts/groomingBooking.handlebars")
+}
+
+const registerBtn = document.getElementById("grooming");
+groomingBtn.addEventListener('click', grooming);
+function grooming(event) {
+    window.open("layouts/groomingBooking.handlebars")
+}
